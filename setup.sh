ee0f27e2869c5ae78251c6b4521d6828261b3b6b
Bryce Tolman <brycealantolman@gmail.com>
2025-06-27 22:50:31 -0600

    update setup

--- setup.sh
+++ setup.sh
@@ -28,3 +28,12 @@ else
 }' > "$setup/batman.lua"
     echo "  ✅ Done"
 fi
+
+# add a line to the init.lua fileAdd commentMore actions
+if ! grep -Fq "require(\"batman\").setup(" "$root/init.lua"; then
+    echo -e "Adding batman to \033[1;32minit\033[0m file in the \033[1;34mroot\033[0m directory"
+    echo -e 'require("batman").setup({})' >> "$root/init.lua"
+    echo -e "  ✅ Done"
+else 
+    echo -e "✅ \033[1;34mroot/\033[0;32minit\033[0m file contains \033[0;32mbatman\033[0m"
+fi
